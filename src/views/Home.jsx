56cafaa6768d7296ca931e7c9a2adf46d7778d2e
Paige <paigehcarroll@gmail.com>
2024-02-06 15:59:52 -0600

    added Single list component to Home page to render user lists

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,5 @@
 import './Home.css';
+import { SingleList } from '../components/SingleList';
 
 export function Home({ data, setListPath }) {
 	return (
@@ -7,10 +8,14 @@ export function Home({ data, setListPath }) {
 				Hello from the home (<code>/</code>) page!
 			</p>
 			<ul>
-				{/**
-				 * TODO: write some JavaScript that renders the `lists` array
-				 * so we can see which lists the user has access to.
-				 */}
+				{data.map((item) => (
+					<SingleList
+						key={item.path}
+						name={item.name}
+						path={item.path}
+						setListPath={setListPath}
+					/>
+				))}
 			</ul>
 		</div>
 	);
