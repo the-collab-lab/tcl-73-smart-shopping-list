f49ce05a23716006be9828bd7e637d50c338813c
jaguilar89 <joseaguilardev@gmail.com>
2024-02-12 15:59:31 -0500

    edit ManageList.jsx

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -1,7 +1,72 @@
+import { useState } from 'react';
+import { addItem } from '../api/firebase';
+
 export function ManageList() {
+	const form = {
+		itemName: '',
+		daysUntilNextPurchase: 0,
+	};
+
+	const [formData, setFormData] = useState(form);
+
+	function handleChange(e) {
+		setFormData({
+			...formData,
+			itemName: e.target.value,
+			daysUntilNextPurchase: e.target.value,
+		});
+		console.log(formData);
+	}
+
 	return (
-		<p>
-			Hello from the <code>/manage-list</code> page!
-		</p>
+		<div>
+			<form>
+				<div>
+					<label htmlFor="item">Item Name</label>
+					<input
+						value={formData.itemName}
+						onChange={handleChange}
+						name="itemName"
+						type="text"
+					/>
+				</div>
+
+				<div>
+					<label htmlFor="purchase-choice">
+						How soon do you think you'll purchase this item again?
+					</label>
+					<ul>
+						<li>
+							<label htmlFor="soon"> Soon (7 days) </label>
+							<input
+								value={7}
+								onChange={handleChange}
+								name="daysUntilNextPurchase"
+								type="radio"
+							/>
+						</li>
+						<li>
+							<label htmlFor="kind-of-soon"> Kind of Soon (14 days)</label>
+							<input
+								value={14}
+								onChange={handleChange}
+								name="daysUntilNextPurchase"
+								type="radio"
+							/>
+						</li>
+						<li>
+							<label htmlFor="not-soon"> Not Soon (30 days)</label>
+							<input
+								value={30}
+								onChange={handleChange}
+								name="daysUntilNextPurchase"
+								type="radio"
+							/>
+						</li>
+					</ul>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
 	);
 }
