4160494221d063d8d77e7309cd73573797bb5070
jaguilar89 <joseaguilardev@gmail.com>
2024-02-16 13:59:16 -0500

    made suggested changes to ManageList.jsx

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -1,7 +1,10 @@
 import { addItem } from '../api/firebase';
+import { useRef } from 'react';
 
 export function ManageList({ listPath }) {
-	async function handleSubmit(e) {
+	const formRef = useRef();
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
 
 		const formData = new FormData(e.target);
@@ -20,56 +23,58 @@ export function ManageList({ listPath }) {
 		} catch (error) {
 			alert('There was a problem');
 		}
-		document.getElementById('item-form').reset();
-	}
+
+		formRef.current.reset();
+	};
 
 	return (
-		<div>
-			<form id="item-form" onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor="item">Item Name</label>
-					<input name="itemName" type="text" id="item" required />
-				</div>
+		<form ref={formRef} onSubmit={handleSubmit}>
+			<div>
+				<label htmlFor="item">Item Name</label>
+				<input name="itemName" type="text" id="item" required />
+			</div>
 
-				<div>
-					<label htmlFor="daysUntilNextPurchase">
-						How soon do you think you'll purchase this item again?
-					</label>
-					<ul>
-						<li>
-							<label htmlFor="soon"> Soon (7 days)</label>
-							<input
-								value={7}
-								name="daysUntilNextPurchase"
-								type="radio"
-								id="soon"
-								required
-							/>
-						</li>
-						<li>
-							<label htmlFor="kind-of-soon"> Kind of Soon (14 days)</label>
-							<input
-								value={14}
-								name="daysUntilNextPurchase"
-								type="radio"
-								id="kind-of-soon"
-								required
-							/>
-						</li>
-						<li>
-							<label htmlFor="not-soon"> Not Soon (30 days)</label>
-							<input
-								value={30}
-								name="daysUntilNextPurchase"
-								type="radio"
-								id="not-soon"
-								required
-							/>
-						</li>
-					</ul>
-				</div>
-				<button type="submit">Submit</button>
-			</form>
-		</div>
+			<div>
+				<label htmlFor="daysUntilNextPurchase">
+					How soon do you think you'll purchase this item again?
+				</label>
+
+				<p>Please select an option:</p>
+
+				<ul>
+					<li>
+						<label htmlFor="soon"> Soon (7 days)</label>
+						<input
+							value={7}
+							name="daysUntilNextPurchase"
+							type="radio"
+							id="soon"
+							required
+						/>
+					</li>
+					<li>
+						<label htmlFor="kind-of-soon"> Kind of Soon (14 days)</label>
+						<input
+							value={14}
+							name="daysUntilNextPurchase"
+							type="radio"
+							id="kind-of-soon"
+							required
+						/>
+					</li>
+					<li>
+						<label htmlFor="not-soon"> Not Soon (30 days)</label>
+						<input
+							value={30}
+							name="daysUntilNextPurchase"
+							type="radio"
+							id="not-soon"
+							required
+						/>
+					</li>
+				</ul>
+			</div>
+			<button type="submit">Submit</button>
+		</form>
 	);
 }
