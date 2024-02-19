8580a5254affaf94b2ed347e9291074f6b3d93a6
jaguilar89 <joseaguilardev@gmail.com>
2024-02-19 18:25:51 -0500

    first commit

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const [searchInput, setSearchInput] = useState('');
+
+	const handleChange = (e) => {
+		setSearchInput(e.target.value);
+	};
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<label htmlFor="item-search">Search for an item:</label>
+			<input
+				id="item-search"
+				type="text"
+				placeholder="Search item..."
+				onChange={handleChange}
+			/>
 			<ul>
 				{data.map((item, index) => (
 					<ListItem name={item.name} key={index} />
