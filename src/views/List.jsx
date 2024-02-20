b4e3c27af1eeed3132dae2907521603691597292
jaguilar89 <joseaguilardev@gmail.com>
2024-02-19 19:32:56 -0500

    added conditional message display for empty list vs no matching items

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -3,7 +3,6 @@ import { ListItem } from '../components';
 
 export function List({ data }) {
 	const [searchInput, setSearchInput] = useState('');
-	const msgToUser = 'There is no items in the list ';
 	const handleChange = (e) => {
 		setSearchInput(e.target.value);
 	};
@@ -17,7 +16,6 @@ export function List({ data }) {
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
-			{!data.length && <p> {msgToUser} </p>}
 
 			<label htmlFor="item-search">Search for an item:</label>
 			<input
@@ -31,8 +29,8 @@ export function List({ data }) {
 					<ListItem name={item.name} key={index} />
 				))}
 			</ul>
-
-			{data.length && !filteredData.length > 0 && (
+			{!data.length && <p>There are no items in this list.</p>}
+			{data.length > 0 && !filteredData.length > 0 && (
 				<p>There are no matching items</p>
 			)}
 		</>
