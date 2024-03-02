77de2a7d1515edcf5757dfeafb267b00783a5f2c
Paige <paigehcarroll@gmail.com>
2024-03-02 13:35:27 -0600

    refactored component with an early return instead of a ternary operator for more readability

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -13,50 +13,49 @@ export function List({ data }) {
 		return item.name.toLowerCase().includes(searchItem.toLowerCase());
 	});
 
+	if (!data.length) {
+		return (
+			<div>
+				<label htmlFor="add-first-item">
+					There are no items in this list. Click this button to add your first
+					items!
+				</label>
+				<Link to="/manage-list">
+					<button id="add-first-item" type="button">
+						Add items
+					</button>
+				</Link>
+			</div>
+		);
+	}
 	return (
-		<>
-			{data.length > 0 ? (
-				<div>
-					<label htmlFor="item-search"> Search for an item: </label>
-					<input
-						id="item-search"
-						type="text"
-						placeholder="Search item..."
-						onChange={handleChange}
-						value={searchItem}
-					/>
-					{searchItem && (
-						<button
-							type="button"
-							name="clearInput"
-							aria-label="Clear input"
-							onClick={() => setSearchItem('')}
-						>
-							X
-						</button>
-					)}
-					<ul>
-						{filteredItems.map((item, index) => (
-							<ListItem name={item.name} key={index} />
-						))}
-					</ul>
-					{data.length > 0 && !filteredItems.length > 0 && (
-						<p>There are no matching items.</p>
-					)}
-				</div>
-			) : (
-				<div>
-					<label htmlFor="add-first-item">
-						There are no items in this list. Click this button to add your first
-						items!
-					</label>
-					<Link to="/manage-list">
-						<button id="add-first-item" type="button">
-							Add items
-						</button>
-					</Link>
-				</div>
+		<div>
+			<label htmlFor="item-search"> Search for an item: </label>
+			<input
+				id="item-search"
+				type="text"
+				placeholder="Search item..."
+				onChange={handleChange}
+				value={searchItem}
+			/>
+			{searchItem && (
+				<button
+					type="button"
+					name="clearInput"
+					aria-label="Clear input"
+					onClick={() => setSearchItem('')}
+				>
+					X
+				</button>
 			)}
-		</>
+			<ul>
+				{filteredItems.map((item, index) => (
+					<ListItem name={item.name} key={index} />
+				))}
+			</ul>
+			{data.length > 0 && !filteredItems.length > 0 && (
+				<p>There are no matching items.</p>
+			)}
+		</div>
 	);
 }
