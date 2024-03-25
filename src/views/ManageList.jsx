5216736692d6df974fc0031d44c6c4df2e3c0ca5
Rachel Spencer <rachelspencer688@gmail.com>
2024-03-25 15:54:13 -0400

    pass listName prop to ManageList component from App.js and render list name with a p tag

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -1,9 +1,10 @@
 import { AddItem } from '../components/AddItem';
 import { ShareList } from '../components/ShareList';
 
-export function ManageList({ listPath, data }) {
+export function ManageList({ listPath, data, listName }) {
 	return (
 		<div>
+			<p>List Name: {listName}</p>
 			<AddItem listPath={listPath} data={data} />
 			<ShareList listPath={listPath} />
 		</div>
