b8b15d2e2402437b4e29ccbc8af54a317f4b3928
“Iryna <maria14trush@gmail.com>
2024-03-25 21:58:52 -0700

    add a new component NotFoundPage

---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFoundPage() {
+	return (
+		<div>
+			<h1>404</h1>
+			<p>Oops! The page you are looking for does not exist.</p>
+		</div>
+	);
+}
