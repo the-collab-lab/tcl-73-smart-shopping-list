86b2c97e8ad3f8bd6863649d75a604388ea1b1cb
“Iryna <maria14trush@gmail.com>
2024-03-25 22:19:21 -0700

    add a navigation link to Home page

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFoundPage() {
 	return (
 		<div>
 			<h1>404</h1>
 			<p>Oops! The page you are looking for does not exist.</p>
+			<p>
+				Go to <Link to="/"> Home Page</Link>
+			</p>
 		</div>
 	);
 }
